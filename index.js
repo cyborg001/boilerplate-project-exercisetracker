a7be9f7c784a855773fe3fcbccf8128ea55969ad
Carlos Ramirez <cgrs27@gmail.com>
2023-01-13 00:24:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,176 +4,129 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-mongoose.set('strictQuery', true)
+// const { createAndSaveUser } = require('./schemas.js')
+const User = require("./schemas.js").User;
+const Exercise = require("./schemas.js").Exercise;
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true
 });
+const TIMEOUT = 10000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// crea un esquema y un model para User
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-});
-const User = mongoose.model('User', userSchema);
-
-// crea un esquema y un model para Exercises
-const exerciseSchema = new mongoose.Schema({
-  user: { type: mongoose.Mixed },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  fecha: Date
-});
-const Exercise = mongoose.model('Exercise', exerciseSchema);
-
-app.post('/api/users', function(req, res) {
-  username = req.body.username;
-  let user = new User({
-    username: username
-  })
-  user.save(function(err, usuario) {
+const createUser = require("./schemas.js").createAndSaveUser;
+app.post("/api/users", function(req, res, next) {
+  createUser(req.body.username, function(err, data) {
     if (err) {
-      return console.log(err);
-
-    } else {
-      console.log('user created.');
-      res.send({ username: username, "_id": usuario._id });
+      return next(err);
     }
-  })
-
-});
-
-app.get('/api/users', function(req, res) {
-  User.find({}, 'username _id', function(err, data) {
-    if (err) return console.log(err);
-    res.send(data)
+    User.findById(data._id, "username _id", function(err, usuario) {
+      if (err) {
+        return next(err);
+      }
+      res.send({ username: usuario.username, _id: usuario._id })
+    })
   })
 })
 
-app.post('/api/users/:_id/exercises', function(req, res) {
-  console.log(req.body[':_id'],'hola')
-  let id = req.body[':_id']
-  let fecha;
-  if (req.body.date) {
-    fecha = new Date(req.body.date);
-  } else {
-    fecha = new Date();
-  }
-  User.findById(id, function(err, user) {
-    if (err) return console.log(err);
-    console.log(user)
-    console.log(fecha)
-    let body = {
-      user: user,
-      description: req.body.description,
-      duration: Number(req.body.duration),
-      fecha: fecha
-    }
-    let exercise = new Exercise(body)
-    exercise.save(function(err, exer) {
-      if (err) {
-        return console.log(err);
-      } else {
-        console.log(exer)
-        res.send({
-          "username": exer.user.username,
-          "description": exer.description,
-          "duration": Number(exer.duration),
-          "date": exer.fecha.toDateString(),
-          "_id": exer.user._id
+
+app.post("/api/users/:_id/exercises", function(req, res, next){
+  if(req.params._id && req.body){
+    var id = req.params._id
+    User.findById(id,'username',function(err, usuario){
+      if (err){
+        return next(err);
+      }
+      if(!usuario){
+        console.log('no hay usuario')
+        req.body = null
+        next();
+      }else{
+        var date = req.body.date ? new Date(req.body.date) : new Date();
+        body = {
+          id: id,
+          username: usuario.username,
+          description: req.body.description,
+          duration: Number(req.body.duration),
+          date: date  
+        }
+        var exercise = new Exercise(body)
+        exercise.save(function(err,data){
+          if (err) return console.log(err)
+          if(data){
+            Exercise.findById(data._id,function(err,exer){
+              if (err) return console.log(err)
+              req.body = {
+                _id: exer.id,
+                username: usuario.username,
+                description: exer.description,
+                duration: exer.duration,
+                date: exer.date.toDateString()
+                
+              }
+              next();
+            })
+          }
         })
       }
-
+        
+      
     })
+  }else{
+    req.params
+    req.body=null
+    next();
+  }
+},function(req, res){
+  res.json(req.body)
+})
 
+
+app.get('/api/users', function(req, res) {
+  User.find({}, 'username', function(err, users) {
+    if (err) throw err;
+    res.json(users)
   })
 })
-// app.post('/api/users/:_id/exercises', function(req, res) {
-//   let fecha;
-//   if (req.body.date) {
-//     fecha = new Date(req.body.date);
-//   } else {
-//     fecha = new Date();
-//   }
-//   console.log(fecha)
-//   let id = req.body.id = req.body[':_id'];
-//   User.findById(id, function(err, usuario) {
-//     if (err) return console.log(err);
-//     console.log(usuario,'-------')
-//     username = usuario.username
 
-//     body = {
-//       id: id,
-//       username: username,
-//       description: req.body.description,
-//       duration: Number(req.body.duration),
-//       fecha: fecha
-//     }
-//     var exercise = new Exercise(body);
-//     exercise.save(function(err, ejercicio) {
-//       if (err) return console.log(err);
-//       console.log('Ejercicio creado', ejercicio);
-//       output = {
-//         username: ejercicio.username,
-//         "description": ejercicio.description,
-//         "duration": ejercicio.duration,
-//         "date": ejercicio.fecha.toDateString(),
-//         "_id": ejercicio.id
-//       }
-//       res.send(output);
-
-//     });
-//   });
-// })
-
-// app.get("/api/users/:_id?/logs", function(req, res) {
-//   let id = req.params._id;
-//   let from = new Date("1 January 1970");
-//   let to = new Date();
-//   let limit = '';
-//   if (req.query.from) {
-//     from = new Date(req.query.from);
-//   }
-//   if (req.query.to) {
-//     to = new Date(req.query.to);
-//   }
-//   if (req.query.limit) {
-//     limit = Number(req.query.limit);
-//   }
-//   console.log(to.toDateString())
-//   console.log(id)
-//   User.findById(id, function(err, usuario) {
-//     if (err) return console.log(err);
-//     let name = usuario.username
-//     query = Exercise.find({ id: id }, 'description duration fecha username-_id').where('fecha').gte(from).lte(to).limit(limit).exec(function(err, data) {
-//       if (err) return console.log(err);
-//       console.log(data)
-//       arr = data.map(element => ({
-//         "description": element.description,
-//         "duration": element.duration,
-//         "date": element.fecha.toDateString()
-//       }))
-//       res.send({
-//         "username": name,
-//         "count": arr.length,
-//         "_id": id,
-//         "log": arr
-//       })
-//     })
-//   })
-// })
-
-
-
+app.get("/api/users/:_id?/logs", function(req, res) {
+  let id = req.params._id;
+  let from = req.query.from || new Date(0) ;
+  let to = req.query.to || new Date(Date.now());
+  let limit = Number(req.query.limit) || 0;
+  console.log(id)
+  User.findById(id, function(err, usuario) {
+    if (err) throw err;
+    if (!usuario) {
+      return console.log(err)
+    }
+    let name = usuario.username
+    Exercise.find({ id: id }, 'description duration date _id').where('date').gte(from).lte(to).limit(limit).exec(function(err, data) {
+      if (err) return console.log(err);
+      arr = data.map(element => ({
+        description: element.description,
+        duration: Number(element.duration),
+        date: element.date.toDateString()
+      }))
+      body = {
+        username: name,
+        count: arr.length,
+        _id: id,
+        log: arr
+      }
+      console.log(body,' logs')
+      res.json(body)
+    })
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
-
-
-
-exports.User = User;
